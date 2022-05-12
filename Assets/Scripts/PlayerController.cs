b44e0ff5457b1e19d06be4c16da0d91969dc9356
bhayden51 <60995047+bhayden51@users.noreply.github.com>
2022-05-11 21:39:04 -0500

    Player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
