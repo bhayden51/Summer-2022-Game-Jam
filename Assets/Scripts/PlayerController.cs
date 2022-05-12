2628fa41e9052e87a67d10b3dde669f392161177
bhayden51 <60995047+bhayden51@users.noreply.github.com>
2022-05-11 21:53:59 -0500

    player guy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject com;
+
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.centerOfMass = com.transform.localPosition;
     }
 
     void Update()
