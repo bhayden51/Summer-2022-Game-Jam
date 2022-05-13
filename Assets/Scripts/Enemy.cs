04ae9f34fca8487e11e170a8b63ba0a8dacc46d5
bhayden51 <60995047+bhayden51@users.noreply.github.com>
2022-05-12 20:35:04 -0500

    Enemy done

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int requiredSpeedToKill;
+    public float redirectTime;
+    public float redirectPower;
+
+    private GameObject player;
+    private Vector2 hitDirection;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    public void Redirect(GameObject ball)
+    {
+        StartCoroutine(HitBall(ball));
+    }
+
+    private IEnumerator HitBall(GameObject ball)
+    {
+        Ball ballScript = ball.GetComponent<Ball>();
+        Time.timeScale = 0;
+        ballScript.arrowPivot.SetActive(true);
+        yield return new WaitForSecondsRealtime(redirectTime);
+        ballScript.arrowPivot.SetActive(false);
+        hitDirection = (player.transform.position - ball.transform.position).normalized;
+        Time.timeScale = 1;
+        LaunchBall(ball);
+    }
+
+    private void LaunchBall(GameObject ball)
+    {
+        Ball ballScript = ball.GetComponent<Ball>();
+        Rigidbody2D ballRb = ball.GetComponent<Rigidbody2D>();
+        float ballSpeed = ballRb.velocity.magnitude;
+        ballRb.velocity = Vector2.zero;
+        ballRb.AddForce(hitDirection * (ballSpeed + redirectPower), ForceMode2D.Impulse);
+        ballScript.bounces = 0;
+    }
+}
--- Assets/Scripts/Hurtbox.cs
+++ Assets/Scripts/Hurtbox.cs
@@ -13,5 +13,15 @@ public class Hurtbox : MonoBehaviour
             ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             Debug.Log("Hurt");
         }
+        if (collision.tag == "Enemy")
+        {
+            Enemy en = collision.gameObject.GetComponent<Enemy>();
+            if (ball.GetComponent<Ball>().speedLevel >= en.requiredSpeedToKill)
+                Destroy(en.gameObject);
+            else
+            {
+                en.Redirect(ball);
+            }
+        }
     }
 }
