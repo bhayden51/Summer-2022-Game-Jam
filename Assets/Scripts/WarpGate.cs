ace2544ec05629482dbd24918391f9587f74de8b
bhayden51 <60995047+bhayden51@users.noreply.github.com>
2022-05-16 22:06:49 -0500

    warp gate

---
+++ Assets/Scripts/WarpGate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpGate : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ball")
+        {
+            Debug.Log("HIT");
+            Ball ballScript = collision.gameObject.GetComponent<Ball>();
+            ballScript.bounces = 0;
+            float newSpeed = ((ballScript.speedLevel * 10) + 10);
+            if (newSpeed > 50)
+                newSpeed = 50;
+            ballScript.rb.velocity = ballScript.rb.velocity.normalized * newSpeed;
+        }
+    }
+}
